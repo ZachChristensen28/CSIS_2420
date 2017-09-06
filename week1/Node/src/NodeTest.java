9f0397e46009ca83065053c8207d6499d43e7721
Zach <zachchristensen28@gmail.com>
2017-09-06 17:35:30 -0600

    updates

--- week1/Node/src/NodeTest.java
+++ week1/Node/src/NodeTest.java
@@ -4,19 +4,13 @@ public class NodeTest {
 
 	public static void main(String[] args) {
 		
-		
-		Character ch = new Character('$');
-		BigInteger bi = new BigInteger("1234567891011121314151617181920");
-		
-		Node n1 = new Node(bi);
-		Node n2 = new Node(ch, n1);
+		Node n1 = new Node(100);
 		
 		for(int i = 99; i > 0; i--) {
 			
 		}
 		
-		System.out.println(n2.getObject());
-		System.out.println(n1.getObject());
-		System.out.println(n2.getNext().getObject());
+		
+		
 	}
 }
