1866a0bd08a65658da739642a61fae5259793bcc
Zach <zachchristensen28@gmail.com>
2017-09-07 17:26:45 -0600

    finished week1

--- week1/Node/src/NodeTest.java
+++ week1/Node/src/NodeTest.java
@@ -1,16 +1,25 @@
-import java.math.BigInteger;
-
+/**
+ * 
+ * @author zacharychristensen
+ *
+ */
 public class NodeTest {
 
 	public static void main(String[] args) {
-		
-		Node n1 = new Node(100);
-		
-		for(int i = 99; i > 0; i--) {
-			
+
+		Node n[] = new Node[101];
+
+		for (int i = 100; i > 0; i--) {
+			for(int j = i; j > 0; j--) {
+				int x = j * j;
+				n[j] = new Node(x);
+			}
+			n[i].setNext(n[i - 1]);
 		}
-		
-		
-		
+		System.out.println("Node 100: " + n[100].getObject());
+		for (int i = 100; i > 1; i--) {
+			int counter = i - 1;
+			System.out.println("Node " + counter  + ": " + n[i].getNext().getObject());
+		}
 	}
 }
