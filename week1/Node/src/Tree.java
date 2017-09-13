6b7306a507bb192728ea453005df9a213dac654b
ZachChristensen28 <ZachChristensen28@users.noreply.github.com>
2017-09-13 13:55:02 -0600

    in class work

---
+++ week1/Node/src/Tree.java
@@ -0,0 +1,32 @@
+
+public class Tree {
+	
+	private TreeNode root;
+	
+	public Tree(int key) {
+		root = new TreeNode(key);
+	}
+
+	public TreeNode getRoot() {
+		return root;
+	}
+	
+	/*
+	 * Insert Node
+	 */
+	public void insertNode(TreeNode t, int d) {
+		if(d < t.getKey()) {
+			if(t.getLeft() == null) {
+				t.setLeft(new TreeNode(d));
+			} else {
+				insertNode(t.getLeft(), d);
+			}
+		} else if(d > t.getKey()) {
+			if(t.getRight() == null) {
+				t.setRight(new TreeNode(d));
+			} else {
+				insertNode(t.getRight(), d);
+			}
+		}
+	}
+}
