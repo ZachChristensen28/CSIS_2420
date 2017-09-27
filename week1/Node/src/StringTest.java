fc232df73a1766c64f81cee56574ecf64c9fcec9
ZachChristensen28 <ZachChristensen28@users.noreply.github.com>
2017-09-27 14:53:41 -0600

    yup

--- week1/Node/src/StringTest.java
+++ week1/Node/src/StringTest.java
@@ -8,6 +8,11 @@ public class StringTest {
 		System.out.println("Comparing Zebra to Antelope: " + s1.compareTo(s2));
 		System.out.println("Comparing Antelope to Zebra: " + s2.compareTo(s1));
 
+		String suuAbout = "Southern Utah University is a caring campus community "
+				+ "where students come to explore their interests and prepare for meaningful careers";
+		
+		
+		
 	}
 
 }
